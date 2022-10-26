61e0e1642c63c22bd4e131877a9ec6a0196cad63
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-26 20:54:24 +0200

    Added a product function

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,4 @@
 
-
 # TODO (wedkarz): add element type checking
 def sum_arr(array: list):
     """ Returns a sum of the array elements """
@@ -12,3 +11,16 @@ def sum_arr(array: list):
     
     return sum
 
+
+# TODO (wedkarz): add element type checking
+def product_arr(array: list):
+    """ Returns a multiplication of the array elements """
+    if type(array) is not list:
+        raise TypeError("Invalid input provided. List type argument expected.")
+    
+    prod = array[0]
+    for element in array:
+        prod *= element
+
+    return prod
+    
