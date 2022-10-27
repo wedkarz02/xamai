fbf97c7e2a3a495c6c87dba75eb1634000217bb3
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-27 19:57:33 +0200

    Fixed type checking issue

--- src/functions.py
+++ src/functions.py
@@ -1,26 +1,31 @@
 
-# TODO (wedkarz): add element type checking
 def sum_arr(array: list):
     """ Returns a sum of the array elements """
+
     if type(array) is not list:
         raise TypeError("Invalid input provided. List type argument expected.")
 
     sum = 0
     for element in array:
+        if type(element) not in [int, float]:
+            raise TypeError("Invalid input provided. Every array element must be a real number.")
+        
         sum += element
     
     return sum
 
 
-# TODO (wedkarz): add element type checking
 def product_arr(array: list):
     """ Returns a multiplication of the array elements """
+
     if type(array) is not list:
         raise TypeError("Invalid input provided. List type argument expected.")
     
     prod = array[0]
     for element in array:
+        if type(element) not in [int, float]:
+            raise TypeError("Invalid input provided. Every array element must be a real number.")
+        
         prod *= element
 
     return prod
-    
