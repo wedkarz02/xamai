281432c35dca2bc3f8e8ab445dc6732d92a2dd51
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-29 18:27:26 +0200

    Removed test code

--- xamai.py
+++ xamai.py
@@ -7,13 +7,3 @@ try:
     from src.functions import *
 except:
     raise ImportError("Module files not found. Make sure all files are included properly.")
-
-from random import randrange, random
-
-min = 0
-max = 100
-lst = [randrange(min, max - 1) + random() for _ in range(15)]
-
-lst_normalized = [normalize(x, min, max) for x in lst]
-print(lst)
-print(lst_normalized)
