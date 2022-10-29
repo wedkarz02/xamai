b8cbcc8a0f803aacc7fafe8d5832e451b32d3297
Paweł Rybak <pawel.rybak13@gmail.com>
2022-10-29 18:28:22 +0200

    Added phi number

--- src/constants.py
+++ src/constants.py
@@ -2,5 +2,6 @@
 # This file declares the most important
 # mathematical constants.
 
-pi = 3.141592653589793
-e = 2.718281828459045
+pi  = 3.141592653589793
+e   = 2.718281828459045
+phi = 1.618033988749894
