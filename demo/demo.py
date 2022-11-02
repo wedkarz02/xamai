b1d2d517f85ca626a258410c975c04622c6bbf11
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-02 16:42:10 +0100

    Changed parent directory import system

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,17 @@
 
-import sys
-sys.path.append("..")
+# Demo file created for functionality testing
 
+import os
+import sys
+import inspect
+
+# Importing a parent directory
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.insert(0, parent_dir)
+
+# Actial demo:
 import xamai
 
 print(xamai.pi)
