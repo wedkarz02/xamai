a3532ac82a2a9a555ea0d8c430fa96e89163f5e6
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-02 17:35:42 +0100

    Tested the normalize function

--- demo/demo.py
+++ demo/demo.py
@@ -11,7 +11,7 @@ parent_dir = os.path.dirname(current_dir)
 
 sys.path.insert(0, parent_dir)
 
-# Actial demo:
+# Actual demo:
 import xamai
 
 print(xamai.pi)
@@ -20,3 +20,8 @@ print(xamai.exponent(2, 6))
 lst = [9, 5, 4, 3, 7, 4, 65, 1, 0]
 xamai.bubble_sort(lst)
 print(lst)
+
+nums = [1, 2, 3, 4, 5]
+nums_normalized = [xamai.normalize(x, 0, 5) for x in nums]
+print(nums)
+print(nums_normalized)
