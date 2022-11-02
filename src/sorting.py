1462e3f418c04b49197a0fed153aa455bb188ff5
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-02 16:02:50 +0100

    Fixed a bug in the bubble sort implementation

--- src/sorting.py
+++ src/sorting.py
@@ -2,7 +2,8 @@
 from functions import swap
 
 def bubble_sort(array: list):
-    for i in range(len(array)):
+    for i in range(len(array) - 1):
         for j in range(len(array) - i - 1):
-            if array[j + 1] > array[j]:
-                swap(array, array[j], array(j + 1))
+            if array[j + 1] < array[j]:
+                swap(array, j, j + 1)
+
