42f3ce9761e13cf0102fcebe473d67e383fef821
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-05 19:19:58 +0100

    Main module now exits after raising an exception

--- xamai.py
+++ xamai.py
@@ -2,9 +2,11 @@
 # Main module importing all of the necessary files
 # Import this module to your project to start using xamai
 
+import sys
+
 try:
     from src.constants import *
     from src.functions import *
     from src.sorting import *
 except ModuleNotFoundError:
-    print("[ERROR]: Module files not found. Make sure all files are included properly.")
+    sys.exit("[ERROR]: Module files not found. Make sure all files are included properly.")
