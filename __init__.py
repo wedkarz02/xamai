7558bc5f2f64f15a9c63d6c4cd9c6cd097fbcedc
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-06 19:34:04 +0100

    Fixed the imports system

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+# Main module initializing the package.
+# This file imports all of the necessary
+# modules to your project.
+# 
+# See REAMDE.md for more information.
+
+from xamai.src.constants import *
+from xamai.src.functions import *
+from xamai.src.sorting import *
