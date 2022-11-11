ea6f0357769db236fe95be92afb4db1a348cec6d
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-11 13:13:05 +0100

    Changed the import in sorting

--- src/sorting.py
+++ src/sorting.py
@@ -1,9 +1,9 @@
 
-from . import functions
+from .functions import swap
 
 def bubble_sort(array: list):
     for i in range(len(array) - 1):
         for j in range(len(array) - i - 1):
             if array[j + 1] < array[j]:
-                functions.swap(array, j, j + 1)
+                swap(array, j, j + 1)
 
