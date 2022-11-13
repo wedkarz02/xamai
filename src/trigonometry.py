c85931e1a250c36fa3af0d87c5452e60c088e847
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-13 17:55:54 +0100

    Created a sine function

---
+++ src/trigonometry.py
@@ -0,0 +1,12 @@
+
+#TODO (wedkarz): Add documentation
+def sin_ts(angle: float, n: int):
+    tmp = angle
+    sine = tmp
+
+    for i in range(1, n):
+        mult = (-angle * angle) / ((2 * i + 1) * (2 * i))
+        tmp *= mult
+        sine += tmp
+    
+    return sine
