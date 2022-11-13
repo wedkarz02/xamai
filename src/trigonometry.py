2d71bb53b21c4df01453cc70dd6894f7542fee8c
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-13 23:44:22 +0100

    Added documentation for the sine function

--- src/trigonometry.py
+++ src/trigonometry.py
@@ -1,6 +1,11 @@
 
-#TODO (wedkarz): Add documentation and result rounding
 def sin_ts(angle: float, n: int):
+    # Returns a sine of the given angle (in radians)
+    # calculated by using a simplified version
+    # of the Taylor series
+    # (without the need for factorial functions)
+    # https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
+
     tmp = angle
     sine = tmp
 
