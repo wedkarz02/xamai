7a9318b4755b30f974bb6d5b73e1e233769caf10
Paweł Rybak <pawel.rybak13@gmail.com>
2022-11-17 15:51:37 +0100

    Added a demo file for general testing

---
+++ demo/general_demo.py
@@ -0,0 +1,15 @@
+
+# Demo file created for general functionality testing.
+# 
+# Adding a parent path of the main package to sys.path
+# See README.md for more information.
+
+import sys
+import pathlib
+
+package_path = str(pathlib.Path(__file__).resolve().parents[2])
+sys.path.insert(0, package_path)
+
+
+# Actual demo starts here:
+import xamai
