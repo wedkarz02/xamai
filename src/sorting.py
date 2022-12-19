2e03a6b31529236f434a8085638df772981726b6
Paweł Rybak <pawel.rybak13@gmail.com>
2022-12-19 15:36:43 +0100

    Added documentation for sorting functions

--- src/sorting.py
+++ src/sorting.py
@@ -3,24 +3,36 @@ from .functions import swap
 
 
 def bubble_sort(array: list):
+    # Sorts the array values using
+    # the Bubble sorting algorithm
+    # https://en.wikipedia.org/wiki/Bubble_sort
+
     for i in range(len(array) - 1):
         for j in range(len(array) - i - 1):
             if array[j + 1] < array[j]:
                 swap(array, j, j + 1)
 
 
-# TODO (wedkarz): add documentation
 def quick_sort(array: list, start: int, end: int):
+    # Sorts the array values using
+    # the Lomuto Partition scheme
+    # of the Quick sorting algorithm
+    # https://en.wikipedia.org/wiki/Quicksort#Lomuto_partition_scheme
+
     if start >= end or start < 0:
         return
 
-    pivot_index = _partition_qsort(array, start, end)
+    pivot_index = _lomuto_partition(array, start, end)
 
     quick_sort(array, pivot_index + 1, end)
     quick_sort(array, start, pivot_index - 1)
 
 
-def _partition_qsort(array: list, start: int, end: int):
+def _lomuto_partition(array: list, start: int, end: int):
+    # Partition function used in the
+    # Lomuto scheme.
+    # Should not be used outside of the quick_sort function
+
     pivot_index = start
     pivot_value = array[end]
 
