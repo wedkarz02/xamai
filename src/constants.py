ff4d59f26b9bcd6c40399fbda817a01d468c993e
Paweł Rybak <pawel.rybak13@gmail.com>
2023-02-25 13:47:32 +0100

    Added Mill's constant

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,7 @@ class Constants:
         self.e     = 2.718281828459045
         self.phi   = 1.618033988749894
         self.sqrt2 = 1.414213562373095
+
+        # Mill's constant
+        # https://en.wikipedia.org/wiki/Mills%27_constant
+        self.mills_const = 1.3063778838630806904686144926
